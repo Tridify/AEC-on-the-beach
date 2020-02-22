7b7b9427750cbdf975438fcda00bfcd786ae794f
Petter <petter.gasstrom@tridify.com>
2020-02-22 22:47:53 +0200

    Added debugging build

---
+++ Assets/AEC-on-the-beach-content/Scripts/AR/CornerSandboxParenter.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CornerType {
+    Red,
+    Green,
+    Blue,
+    Purple
+}
+public class CornerSandboxParenter : MonoBehaviour {
+
+    public CornerType cornerType = CornerType.Red;
+    public Vector3 sandboxOffset = Vector3.zero;
+
+    private GameObject sandbox;
+
+    [SerializeField]
+    [Tooltip("Sandbox side length in meters")]
+    private float sandboxSideLength = 0.75f;
+
+    [SerializeField]
+    [Tooltip("Sandbox side height in meters")]
+    private float sandboxHeight = 0.2f;
+
+    const float qrCodeSideLength = 0.1f;
+
+
+    private Transform planeParent;
+
+
+    private void Awake() {
+        sandbox = GameObject.Find("Sandbox");
+        // Get the only rotating object as parent
+        planeParent = transform.GetChild(0).transform.GetChild(0);
+    }
+
+    public void SetCornerTypeFromString(string name) {
+        switch (name) {
+            case "Red":
+                cornerType = CornerType.Red;
+                break;
+            case "Green":
+                cornerType = CornerType.Green;
+                break;
+            case "Blue":
+                cornerType = CornerType.Blue;
+                break;
+            case "Purple":
+                cornerType = CornerType.Purple;
+                break;
+            default:
+                cornerType = CornerType.Red;
+                break;
+        }
+
+        AssignOffset();
+        SetSandboxAsChild();
+    }
+
+    public string GetName() {
+        switch (cornerType) {
+            case CornerType.Red:
+                return "Red";
+            case CornerType.Green:
+                return "Green";
+            case CornerType.Blue:
+                return "Blue";
+            case CornerType.Purple:
+                return "Purple";
+            default:
+                return "Red";
+        }
+    }
+
+    public void AssignOffset() {
+        float sideOffset = (sandboxSideLength * 0.5f) + (qrCodeSideLength * 0.5f);
+        float heightOffset = -sandboxHeight;
+        switch (cornerType) {
+            case CornerType.Red:
+                sandboxOffset = new Vector3(-sideOffset, heightOffset, sideOffset); //Lower Right
+                break;
+            case CornerType.Green:
+                sandboxOffset = new Vector3(sideOffset, heightOffset, -sideOffset); //Upper Left
+                break;
+            case CornerType.Blue:
+                sandboxOffset = new Vector3(sideOffset, heightOffset, sideOffset); //Upper Right
+                break;
+            case CornerType.Purple:
+                sandboxOffset = new Vector3(-sideOffset, heightOffset, -sideOffset); //Lower Left
+                break;
+            default:
+                sandboxOffset = new Vector3(-sideOffset, heightOffset, sideOffset); //Lower Right
+                break;
+        }
+    }
+
+    public void SetSandboxAsChild() {
+        if(sandbox.transform.parent != planeParent) {
+            sandbox.transform.SetParent(planeParent);
+            sandbox.transform.rotation = Quaternion.identity;
+            sandbox.transform.localPosition = Vector3.zero;
+            sandbox.transform.Translate(sandboxOffset);
+        }
+    }
+
+    void Update() {
+        if (Input.touchCount >= 2) {
+            if ((Input.touches[0].phase == TouchPhase.Began) || (Input.touches[1].phase == TouchPhase.Began)) {
+                string name = GetName();
+                Debug.Log(name + " parentName:" + transform.parent.name);
+                Debug.Log(name + " position:" + transform.position);
+            }
+        }
+    }
+}
---
+++ Assets/AEC-on-the-beach-content/Scripts/AR/CustomTrackedImageInfoManager.cs
@@ -0,0 +1,96 @@
+﻿    using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+/// This component listens for images detected by the <c>XRImageTrackingSubsystem</c>
+/// and overlays some information as well as the source Texture2D on top of the
+/// detected image.
+/// </summary>
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class CustomTrackedImageInfoManager : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("If an image is detected but no source texture can be found, this texture is used instead.")]
+    Texture2D m_DefaultTexture;
+
+    /// <summary>
+    /// If an image is detected but no source texture can be found,
+    /// this texture is used instead.
+    /// </summary>
+    /// 
+    private Camera m_MainCamera;
+    public Texture2D defaultTexture
+    {
+        get { return m_DefaultTexture; }
+        set { m_DefaultTexture = value; }
+    }
+
+    ARTrackedImageManager m_TrackedImageManager;
+
+
+    private List<CornerSandboxParenter> m_parenters = new List<CornerSandboxParenter>();
+
+
+    void Awake() {
+        m_MainCamera = Camera.main;
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    void OnEnable() {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable() {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void UpdateInfo(ARTrackedImage trackedImage, Texture2D texture = null) {
+        var planeParentGo = trackedImage.transform.GetChild(0).gameObject;
+        var planeGo = planeParentGo.transform.GetChild(0).gameObject;
+
+        if (texture) {
+            // Set the texture
+            trackedImage.name = texture.name;
+            CornerSandboxParenter sender = trackedImage.GetComponentInChildren<CornerSandboxParenter>();
+            m_parenters.Add(sender);
+
+            sender.SetCornerTypeFromString(texture.name);
+
+            var material = planeGo.GetComponentInChildren<MeshRenderer>().material;
+            material.mainTexture = texture;
+        }
+
+        // Disable the visual plane if it is not being tracked
+        if (trackedImage.trackingState != TrackingState.None) {
+            planeGo.SetActive(true);
+
+            // The image extents is only valid when the image is being tracked
+            trackedImage.transform.localScale = new Vector3(trackedImage.size.x, trackedImage.size.x, trackedImage.size.y);
+        } else {
+            planeGo.SetActive(false);
+        }
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs) {
+        foreach (var trackedImage in eventArgs.added) {
+            UpdateInfo(trackedImage, trackedImage.referenceImage.texture);
+        }
+
+        float closestDistance = float.MaxValue;
+        int closestIndex = 0;
+        int len = eventArgs.updated.Count;
+        for (int i = 0; i < len; i++) {
+
+            UpdateInfo(eventArgs.updated[i]);
+            float currDistance = Vector3.SqrMagnitude(m_MainCamera.transform.position - eventArgs.updated[i].transform.position);
+            if (currDistance < closestDistance) {
+                closestDistance = currDistance;
+                closestIndex = i;
+            }
+        }
+        m_parenters[closestIndex].SetSandboxAsChild();
+    }
+}
