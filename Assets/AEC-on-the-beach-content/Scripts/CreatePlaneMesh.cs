04f5891aeb67d653a439e91edf817279e257e2cb
m2xai <brett@m2x.ai>
2020-02-22 23:57:08 -0800

    Building.cs recovery.

--- Assets/AEC-on-the-beach-content/Scripts/CreatePlaneMesh.cs
+++ Assets/AEC-on-the-beach-content/Scripts/CreatePlaneMesh.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class CreatePlaneMesh : MonoBehaviour
 {
 	private Mesh mesh;
+    private bool _useDebugData = false;
+
 	private int xSize = 640;
     private int zSize = 480;
+
     public AnimationCurve meshHeightCurve;
+
 	public Gradient terranColorsGradient;
+
     private Vector3[] vertices;
     private Color[] colorMap;
+
     public CreateCubesRandomly CCR;
     public DepthView DV;
-    private float time = 0;
-    private float timeToBuild = 1;
 
     public float separation;  // Percent of Parcel Size that separates parcels.
     public float scale = 0.02f;   //  This is the real life conversion from grid size to Unity space.  Taken from scale factor on parent object.
@@ -24,28 +31,61 @@ public class CreatePlaneMesh : MonoBehaviour
 
     public Building[] Buildings;
 
+    private float time = 0;
+    private float timeToBuild = 1;
+
+    private float[] _testData;
+
     private void Awake() {
-        DV.ConnectToTcpServer();
+
+        
+        if (_useDebugData)
+        {
+            _testData = Noise.GenerateNoiseMap(640, 480, 234561345, 100f, 1, 1, 1, Vector2.zero)
+                .Select(d => d * 200f + 300f)
+                .ToArray();
+            Debug.Log("min " + _testData.Min() + " max " + _testData.Max());
+        }
+        else
+        {
+            DV.ConnectToTcpServer();
+        }
+        
         MeshData meshData = GenerateTerrainMesh();
         mesh = meshData.CreateMesh();
         GetComponent<MeshFilter>().mesh = mesh;
         //CCR.CreateHousesInBegin(mesh.vertices, mesh.normals, xSize, zSize);
+        //CCR.CreateHousesInBegin(mesh.vertices, mesh.normals, xSize, zSize);
 
         //  Procedural City Variables
-        gridPointsPerParcel = 10; 
-        separation = 0.2f;  
-        scale = 0.02f;  
+        gridPointsPerParcel = 10;
+        separation = 0.2f;
+        scale = 0.02f;
         bldgAspectRatio = 3f;
         elevationTolerance = 1f;
         maxElevationDelta = 0.1f;  // Figure 10 centimeters;
 
         UpdateMeshes(DV.GetHeightMap());
         PlaceCity();
+
+
+
     }
 
     private void Update()
     {
-        UpdateMeshes(DV.GetHeightMap());
+        float[] heightMap;
+        if (_useDebugData)
+        {
+            heightMap = _testData;
+        }
+        else
+        {
+            heightMap = DV.GetHeightMap();
+        }
+
+        UpdateMeshes(heightMap);
+
         if(time >= timeToBuild)
         {
             StartCoroutine("Build");
@@ -54,10 +94,10 @@ public class CreatePlaneMesh : MonoBehaviour
         time += Time.deltaTime;
     }
 
+
     IEnumerator Build()
     {
-        // CCR.Checkhouses(mesh.vertices, mesh.normals, xSize, zSize);
-
+        CCR.Checkhouses(mesh.vertices, mesh.normals, xSize, zSize);
         yield return null;
     }
 
@@ -144,105 +184,105 @@ public class CreatePlaneMesh : MonoBehaviour
             return mesh;
         }
     }
-
-    // Use this for initialization
-    public void PlaceCity()
-    {
-        int gridX = Mathf.FloorToInt(xSize / (gridPointsPerParcel + gridPointsPerParcel * separation));
-        int gridZ = Mathf.FloorToInt(zSize / (gridPointsPerParcel + gridPointsPerParcel * separation));
-
-        Buildings = new Building[gridX * gridZ];
-
-        float realOffset = scale * (gridPointsPerParcel) * (1 + separation);    
-
-        for (int z = 0; z < gridZ; z++)
+        public void PlaceCity()
         {
-            for (int x = 0; x < gridX; x++)
+            int gridX = Mathf.FloorToInt(xSize / (gridPointsPerParcel + gridPointsPerParcel * separation));
+            int gridZ = Mathf.FloorToInt(zSize / (gridPointsPerParcel + gridPointsPerParcel * separation));
+
+            Buildings = new Building[gridX * gridZ];
+
+            float realOffset = scale * (gridPointsPerParcel) * (1 + separation);
+
+            for (int z = 0; z < gridZ; z++)
             {
-                Vector3 offset = new Vector3(x * realOffset + gridPointsPerParcel * scale / 2, 0f, z * realOffset + gridPointsPerParcel * scale / 2);
-                Buildings[z * gridX + x] = PlaceBuildingSingle(offset, Mathf.RoundToInt(x * realOffset + gridPointsPerParcel / 2 + z * realOffset + gridPointsPerParcel / 2));
+                for (int x = 0; x < gridX; x++)
+                {
+                    Vector3 offset = new Vector3(x * realOffset + gridPointsPerParcel * scale / 2, 0f, z * realOffset + gridPointsPerParcel * scale / 2);
+                    Buildings[z * gridX + x] = PlaceBuildingSingle(offset, Mathf.RoundToInt(x * realOffset + gridPointsPerParcel / 2 + z * realOffset + gridPointsPerParcel / 2));
+                }
+            }
+
+            /// <summary>
+            /// Places a single self-building object
+            /// </summary>
+            Building PlaceBuildingSingle(Vector3 loc, int MeshIndex)
+            {
+                Vector3 center = transform.position + loc;
+                string bldgName = "Building" + MeshIndex;
+
+                GameObject go = new GameObject();
+
+                go.AddComponent<MeshRenderer>();
+                go.AddComponent<MeshFilter>();
+
+                Building bldg = go.AddComponent<Building>();
+                bldg.size = gridPointsPerParcel * scale;
+                bldg.height = gridPointsPerParcel * scale * bldgAspectRatio;
+
+                /*
+
+                Vector3 lowHighAvg = ElevationData(MeshIndex);
+                Debug.Log(lowHighAvg);
+
+                if (lowHighAvg.x >= 0)
+                {
+                    bldg.Build();
+                    center.y = lowHighAvg.z;
+                    float diff = lowHighAvg.y - lowHighAvg.x;
+                    diff = diff / maxElevationDelta;    // Normalize. 
+                    bldg.size = (1 - diff) * bldg.size;
+                }
+                */
+
+                go.transform.position = center;
+                go.transform.parent = transform;
+
+                bldg.Build();
+                return bldg;
+
             }
         }
 
         /// <summary>
-        /// Places a single self-building object
+        /// Returns a vector 3 with the low, high, and average of the corners points centered on the Mesh Grid reference.
         /// </summary>
-         Building PlaceBuildingSingle(Vector3 loc, int MeshIndex)
-         {
-            Vector3 center = transform.position + loc;
-            string bldgName = "Building" + MeshIndex;
-
-            GameObject go = new GameObject();
-
-            go.AddComponent<MeshRenderer>();
-            go.AddComponent<MeshFilter>();
-
-            Building bldg = go.AddComponent<Building>();
-            bldg.size = gridPointsPerParcel * scale;
-            bldg.height = gridPointsPerParcel * scale * bldgAspectRatio;
-
-            /*
-            
-            Vector3 lowHighAvg = ElevationData(MeshIndex);
-            Debug.Log(lowHighAvg);
-
-            if (lowHighAvg.x >= 0)
-            {
-                bldg.Build();
-                center.y = lowHighAvg.z;
-                float diff = lowHighAvg.y - lowHighAvg.x;
-                diff = diff / maxElevationDelta;    // Normalize. 
-                bldg.size = (1 - diff) * bldg.size;
-            }
-            */
-
-            go.transform.position = center;
-            go.transform.parent = transform;
-
-            bldg.Build();
-            return bldg;
-            
-         }
-    }
-
-    /// <summary>
-    /// Returns a vector 3 with the low, high, and average of the corners points centered on the Mesh Grid reference.
-    /// </summary>
-    public Vector3 ElevationData(int GridReference)
-    {
-        float[] elevations = new float[gridPointsPerParcel * gridPointsPerParcel];
-        for (int i = 0; i < gridPointsPerParcel; i++)
+        public Vector3 ElevationData(int GridReference)
         {
-            for(int j = 0; j < gridPointsPerParcel; j++)
+            float[] elevations = new float[gridPointsPerParcel * gridPointsPerParcel];
+            for (int i = 0; i < gridPointsPerParcel; i++)
             {
-                //  Exception handling for weird elevation data.
-                int convertedIndex = GridReference - gridPointsPerParcel * xSize * i / 2 - gridPointsPerParcel / 2 + j;
-                Debug.Log(convertedIndex);
+                for (int j = 0; j < gridPointsPerParcel; j++)
+                {
+                    //  Exception handling for weird elevation data.
+                    int convertedIndex = GridReference - gridPointsPerParcel * xSize * i / 2 - gridPointsPerParcel / 2 + j;
+                    Debug.Log(convertedIndex);
 
-                float rawElevation = mesh.vertices[GridReference - gridPointsPerParcel * xSize * (gridPointsPerParcel - i) / 2 - gridPointsPerParcel / 2 + j].y;
+                    float rawElevation = mesh.vertices[GridReference - gridPointsPerParcel * xSize * (gridPointsPerParcel - i) / 2 - gridPointsPerParcel / 2 + j].y;
 
-                if (rawElevation < -0.02f || rawElevation > elevationTolerance) rawElevation = 0;
+                    if (rawElevation < -0.02f || rawElevation > elevationTolerance) rawElevation = 0;
 
-                elevations[i * gridPointsPerParcel + j] = rawElevation;
+                    elevations[i * gridPointsPerParcel + j] = rawElevation;
+                }
             }
-        }
-        float sum = 0;
-        int err = 0;
-        float high = 0;
-        float low = 0;
+            float sum = 0;
+            int err = 0;
+            float high = 0;
+            float low = 0;
 
-        for (var i = 0; i < elevations.Length; i++)
-        {
-            if (elevations[i] == 0) err++;
-            else
+            for (var i = 0; i < elevations.Length; i++)
             {
-                sum += elevations[i];
-                if (elevations[i] < low) low = elevations[i];
-                if (elevations[i] > high) high = elevations[i];
-            }
+                if (elevations[i] == 0) err++;
+                else
+                {
+                    sum += elevations[i];
+                    if (elevations[i] < low) low = elevations[i];
+                    if (elevations[i] > high) high = elevations[i];
+                }
 
+            }
+            Vector3 result = new Vector3(low, high, sum / elevations.Length - err);
+            return result;
         }
-        Vector3 result = new Vector3(low, high, sum / elevations.Length - err);
-        return result;
-    }
+
 }
+
